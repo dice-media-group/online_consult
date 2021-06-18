7e7b059c3800322006efa34360b517129a62ef48
captproton <carl@wdwhub.net>
2021-06-17 20:07:08 -0700

    CHANGE: added links from backstage index calendar to bookings page

--- app/controllers/backstage_controller.rb
+++ app/controllers/backstage_controller.rb
@@ -3,12 +3,7 @@ class BackstageController < ApplicationController
     @meetings = Meeting.coming_up
   end
 
-  def bookings
-    
-  end
+  def bookings; end
 
-
-  def analytics
-    
-  end
+  def analytics; end
 end
