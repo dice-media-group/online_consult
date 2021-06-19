1e2b21ed8dd1bedf973072f18ad1e89923bc157d
captproton <carl@wdwhub.net>
2021-06-19 13:49:14 -0700

    CHANGE: added day calendar for bookings

--- app/controllers/backstage_controller.rb
+++ app/controllers/backstage_controller.rb
@@ -3,7 +3,10 @@ class BackstageController < ApplicationController
     @meetings = Meeting.coming_up
   end
 
-  def bookings; end
+  def bookings
+    chosen_date = params[:start_date] || Date.today.to_s
+    @day_calendar_meetings = Meeting.find_all_starting_on_date(chosen_date)
+  end
 
   def analytics; end
 end
--- app/models/meeting.rb
+++ app/models/meeting.rb
@@ -2,14 +2,18 @@ class Meeting < ApplicationRecord
   belongs_to :user
   has_many :comments, dependent: :destroy
 
-  scope :coming_up, -> { where("start_time > ?", Time.now).order(start_time: :asc).first(2) }
-private
+  scope :coming_up, -> { where('start_time > ?', Time.now).order(start_time: :asc).first(2) }
 
+  def self.find_all_starting_on_date(date)
+    selected_date = date.to_s.to_date
+    where(start_time: selected_date.beginning_of_day..selected_date.end_of_day)
+      .order(start_time: :desc)
+  end
+
+  private
 
   def consult_has_meetings
-    unless order.meeting_limit > 0
-      errors.add_to_base 'You have reached your limit of meetings for this consultation.'
-    end
+    errors.add_to_base 'You have reached your limit of meetings for this consultation.' unless order.meeting_limit > 0
   end
 
   def generate_token
@@ -19,5 +23,4 @@ private
   def decrement_meeting_count
     sender.decrement! :invitation_limit
   end
-
 end
