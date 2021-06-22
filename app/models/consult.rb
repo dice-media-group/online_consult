64f562df6b4fee825154fcc2ec8644ab4a2262cd
captproton <carl@wdwhub.net>
2021-06-21 17:19:38 -0700

    CHANGE: styling of app/models/consult.rb

--- app/models/consult.rb
+++ app/models/consult.rb
@@ -1,10 +1,9 @@
 class Consult < ApplicationRecord
-    has_many_attached   :images
-    has_many            :orders
-    has_rich_text       :description
+  has_many_attached   :images
+  has_many            :orders
+  has_rich_text       :description
 
-    validates   :sku, uniqueness: true
-    validates   :title, uniqueness: true
-    validates   :title, :sku, :meeting_limit, :minutes_per_meeting, presence: true
-
+  validates   :sku, uniqueness: true
+  validates   :title, uniqueness: true
+  validates   :title, :sku, :meeting_limit, :minutes_per_meeting, presence: true
 end
