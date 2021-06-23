bb9b86a232e4fed6cc0fc2899370c5bc2201afc9
captproton <carl@wdwhub.net>
2021-06-22 19:04:32 -0700

    CHANGE: customized pasted in features/display_bookings_in_calendar.feature

--- features/step_definitions/calendar_steps.rb
+++ features/step_definitions/calendar_steps.rb
@@ -1,16 +1,16 @@
-Given("I navigate to the index") do
-  root_path
+Given('I navigate to the meeting') do
+  meeting_rooms_path
 end
 
 Then(/^I should see a calender$/) do
   expect(page).to have_css '.simple-calendar'
 end
 
-Then(/^I should( not)? see a calendar link "([^"]*)" at ([^"]*)$/) do |negate, time, date|
+Then(/^I should( not)? see a calendar link "([^"]*)" at ([^"]*)$/) do |negate, time, _date|
   section = find('.today')
   if negate
-    expect(section.has_css? 'a', text: time).to eq false
+    expect(section.has_css?('a', text: time)).to eq false
   else
-    expect(section.has_css? 'a', text: time).to eq true
+    expect(section.has_css?('a', text: time)).to eq true
   end
 end
